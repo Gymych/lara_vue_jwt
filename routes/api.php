42f514ff15888ce841c3135f0da9ecdffd1d9c79
Gymych <gymych@gmail.com>
2022-04-23 00:37:52 +0300

    JWT Installed

--- routes/api.php
+++ routes/api.php
@@ -14,12 +14,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::group(['middleware' => 'api','prefix' => 'auth'], function ($router) {
+    Route::post('login', 'App\Http\Controllers\AuthController@login');
+    Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+    Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
+    Route::post('me', 'App\Http\Controllers\AuthController@me');
 
+    Route::group(['namespace'=> 'App\Http\Controllers\Fruit', 'prefix' => 'fruits'], function (){
+        Route::get('/', 'IndexController');
+    });
+});
 
 Route::group(['namespace'=> 'App\Http\Controllers\User', 'prefix' => 'users'], function (){
     Route::post('/', 'StoreController');
 });
 
-Route::group(['namespace'=> 'App\Http\Controllers\Fruit', 'prefix' => 'fruits'], function (){
-    Route::get('/', 'IndexController');
-});
+
